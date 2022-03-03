9183d534a9ce6bc778f933ea39d2b6ee227334f5
Zlatin Kostov <zlatin@zitech.dev>
2022-03-03 11:44:59 +0200

    retrieve versioning from ci

---
+++ buildSrc/src/main/kotlin/AppVersioning.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022 Zitech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+object AppVersioning {
+
+    private const val MAJOR = 0
+    private const val MINOR = 0
+    private const val PATCH = 1
+
+    const val COMPILE_SDK = 32
+    const val MIN_SDK = 29
+    const val TARGET_SDK = 32
+
+    private const val LOCAL_BUILD_VERSION_CODE = 424242
+
+    fun retrieveVersionName(): String {
+        val versionName = listOf(MAJOR, MINOR, PATCH).joinToString(separator = ".")
+
+        println("AppVersioning. versionName = $versionName")
+
+        return versionName
+    }
+
+    fun retrieveVersionCode(): Int {
+        val versionCode = System.getenv("GITHUB_RUN_ID")?.toInt() ?: LOCAL_BUILD_VERSION_CODE
+
+        println("AppVersioning, versionCode = $versionCode")
+
+        return versionCode
+    }
+}
