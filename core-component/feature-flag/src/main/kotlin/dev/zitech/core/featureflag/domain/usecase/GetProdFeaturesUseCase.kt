67b793841886c1a16220099aa23450a06b62a410
Zlatin Kostov <zlatin@zitech.dev>
2022-03-21 15:03:06 +0200

    add usecases for feature flags

---
+++ core-component/feature-flag/src/main/kotlin/dev/zitech/core/featureflag/domain/usecase/GetProdFeaturesUseCase.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2022 Zitech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.zitech.core.featureflag.domain.usecase
+
+import dev.zitech.core.featureflag.domain.model.ProdFeature
+import dev.zitech.core.featureflag.domain.repository.FeatureFlagRepository
+import javax.inject.Inject
+
+class GetProdFeaturesUseCase @Inject constructor(
+    private val featureFlagRepository: FeatureFlagRepository
+) {
+
+    suspend operator fun invoke(): List<Pair<ProdFeature, Boolean>> {
+        val features = mutableListOf<Pair<ProdFeature, Boolean>>()
+
+        featureFlagRepository.getProdFeatures().forEach { feature ->
+            features.add(
+                Pair(
+                    feature,
+                    featureFlagRepository.isFeatureEnabled(feature),
+                )
+            )
+        }
+
+        return features.sortedBy { it.first.title }
+    }
+}
