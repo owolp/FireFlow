f6b660e0f175fd5d7c1861ea4e5d6e71770e9f5f
Zlatin Kostov <zlatin@zitech.dev>
2022-04-14 09:40:14 +0300

    add strings provider

---
+++ core-component/common/src/main/kotlin/dev/zitech/core/common/framework/strings/StringsProvider.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Zitech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.zitech.core.common.framework.strings
+
+import android.content.Context
+import javax.inject.Inject
+
+interface StringsProvider {
+    operator fun invoke(resId: Int): String
+    fun get(resId: Int, vararg args: CharSequence): String
+}
+
+internal class StringsProviderImpl @Inject constructor(
+    private val context: Context
+) : StringsProvider {
+
+    override fun invoke(resId: Int): String =
+        context.getString(resId)
+
+    override fun get(resId: Int, vararg args: CharSequence): String =
+        context.getString(resId, args)
+}
