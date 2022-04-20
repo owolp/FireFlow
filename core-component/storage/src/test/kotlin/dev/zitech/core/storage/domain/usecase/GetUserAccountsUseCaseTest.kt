649ed71839539c14cb52834bb7ade2d300967c23
Zlatin Kostov <zlatin@zitech.dev>
2022-04-20 15:19:42 +0300

    add GetUserAccountsUseCaseTest

---
+++ core-component/storage/src/test/kotlin/dev/zitech/core/storage/domain/usecase/GetUserAccountsUseCaseTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2022 Zitech
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.zitech.core.storage.domain.usecase
+
+import com.google.common.truth.Truth.assertThat
+import dev.zitech.core.common.DataFactory
+import dev.zitech.core.common.domain.model.DataResult
+import dev.zitech.core.common.framework.strings.StringsProvider
+import dev.zitech.core.storage.data.database.mapper.UserAccountMapper
+import dev.zitech.core.storage.data.database.repository.UserAccountRepositoryImpl
+import dev.zitech.core.storage.framework.database.dao.FakeUserAccountDao
+import dev.zitech.core.storage.framework.database.user.UserAccountDatabaseSource
+import dev.zitech.core.storage.framework.database.user.UserAccountDatabaseSourceImpl
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+internal class GetUserAccountsUseCaseTest {
+
+    private val userAccountDatabaseSource: UserAccountDatabaseSource = UserAccountDatabaseSourceImpl(
+        FakeUserAccountDao(),
+        UserAccountMapper()
+    )
+    private val mockedUserAccountDatabaseSource = mockk<UserAccountDatabaseSource>()
+    private val mockedStringsProvider = mockk<StringsProvider>()
+
+    @Test
+    @DisplayName("WHEN there is no exception THEN return Success")
+    fun success() = runBlocking {
+        // Arrange
+        userAccountDatabaseSource.saveUserAccount(false)
+        userAccountDatabaseSource.saveUserAccount(false)
+        userAccountDatabaseSource.saveUserAccount(true)
+        val sut = GetUserAccountsUseCase(
+            UserAccountRepositoryImpl(
+                userAccountDatabaseSource,
+                mockedStringsProvider
+            )
+        )
+
+        // Act
+        val result = sut()
+
+        // Assert
+        assertThat((result as DataResult.Success).value).hasSize(3)
+    }
+
+    @Test
+    @DisplayName("WHEN there is exception THEN return Error")
+    fun error() = runBlocking {
+        // Arrange
+        val exception = DataFactory.createException()
+        coEvery { mockedUserAccountDatabaseSource.getUserAccounts() } throws exception
+
+        val sut = GetUserAccountsUseCase(
+            UserAccountRepositoryImpl(
+                mockedUserAccountDatabaseSource,
+                mockedStringsProvider
+            )
+        )
+
+        // Act
+        val result = sut()
+
+        // Assert
+        assertThat((result as DataResult.Error).cause).isEqualTo(exception)
+    }
+}
