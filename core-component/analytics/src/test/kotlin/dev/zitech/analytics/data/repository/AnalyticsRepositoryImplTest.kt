a6c4b47f3c10fbd8eaea4fb318108ca817d26997
Zlatin Kostov <zlatin@zitech.dev>
2022-05-03 10:38:55 +0300

    [69] add unit tests

---
+++ core-component/analytics/src/test/kotlin/dev/zitech/analytics/data/repository/AnalyticsRepositoryImplTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2022 Zitech Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package dev.zitech.analytics.data.repository
+
+import com.google.common.truth.Truth.assertThat
+import dev.zitech.analytics.domain.repository.AnalyticsRepository
+import dev.zitech.analytics.framework.analytics.FakeRemoteAnalytics
+import dev.zitech.analytics.framework.source.AnalyticsProviderSourceImpl
+import dev.zitech.core.common.DataFactory
+import org.junit.Before
+import org.junit.Test
+
+internal class AnalyticsRepositoryImplTest {
+
+    private val remoteAnalytics = FakeRemoteAnalytics()
+    private val analyticsProviderSource = AnalyticsProviderSourceImpl(
+        remoteAnalytics
+    )
+
+    private lateinit var sut: AnalyticsRepository
+
+    @Before
+    fun setup() {
+        sut = AnalyticsRepositoryImpl(
+            analyticsProviderSource
+        )
+    }
+
+    @Test
+    fun allowPersonalizedAds() {
+        // Arrange
+        val expectedResult = DataFactory.createRandomBoolean()
+
+        // Act
+        sut.allowPersonalizedAds(expectedResult)
+
+        // Assert
+        assertThat(remoteAnalytics.allowPersonalizedAdsValue).isEqualTo(expectedResult)
+    }
+
+    @Test
+    fun setCollectionEnabled() {
+        // Arrange
+        val expectedResult = DataFactory.createRandomBoolean()
+
+        // Act
+        sut.setCollectionEnabled(expectedResult)
+
+        // Assert
+        assertThat(remoteAnalytics.setCollectionEnabledValue).isEqualTo(expectedResult)
+    }
+}
