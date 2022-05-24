b2f276ce400c482d4437c6ec4fb5264444af4d73
Zlatin Kostov <zlatin@zitech.dev>
2022-05-24 10:59:37 +0300

    fix imports

--- core-component/common/src/debug/kotlin/dev/zitech/core/common/framework/logger/ErrorTreeImpl.kt
+++ core-component/common/src/debug/kotlin/dev/zitech/core/common/framework/logger/ErrorTreeImpl.kt
@@ -15,8 +15,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package dev.zitech.core.common.domain.logger
+package dev.zitech.core.common.framework.logger
 
+import dev.zitech.core.common.domain.logger.ErrorTree
 import javax.inject.Inject
 import timber.log.Timber
 
