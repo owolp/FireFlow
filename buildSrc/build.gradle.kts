c7b3ae0cbdec527d604749be57c9e90c0b9f1da4
Zlatin Kostov <zlatin@zitech.dev>
2022-11-29 14:49:33 +0200

    update library versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -28,6 +28,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.43.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
 }
