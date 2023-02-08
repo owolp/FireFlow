13a89928648318985fd0052dfa0a229110ce6574
Zlatin Kostov <zlatin@zitech.dev>
2023-02-08 22:55:36 +0200

    save account to db

--- feature-component/onboarding/src/main/kotlin/dev/zitech/onboarding/presentation/oauth/viewmodel/OauthViewModel.kt
+++ feature-component/onboarding/src/main/kotlin/dev/zitech/onboarding/presentation/oauth/viewmodel/OauthViewModel.kt
@@ -25,6 +25,7 @@ import dev.zitech.core.common.domain.logger.Logger
 import dev.zitech.core.common.domain.model.DataResult
 import dev.zitech.core.common.domain.model.exception.NoBrowserInstalledException
 import dev.zitech.core.common.presentation.architecture.MviViewModel
+import dev.zitech.core.persistence.domain.usecase.database.SaveUserAccountUseCase
 import dev.zitech.onboarding.domain.usecase.IsOauthLoginInputValidUseCase
 import dev.zitech.onboarding.domain.validator.ClientIdValidator
 import dev.zitech.onboarding.presentation.oauth.viewmodel.resource.OauthStringsProvider
@@ -36,6 +37,7 @@ import kotlinx.coroutines.launch
 internal class OauthViewModel @Inject constructor(
     private val stateHandler: OauthStateHandler,
     private val isOauthLoginInputValidUseCase: IsOauthLoginInputValidUseCase,
+    private val saveUserAccountUseCase: SaveUserAccountUseCase,
     private val clientIdValidator: ClientIdValidator,
     private val oauthStringsProvider: OauthStringsProvider
 ) : ViewModel(), MviViewModel<OauthIntent, OauthState> {
@@ -69,23 +71,40 @@ internal class OauthViewModel @Inject constructor(
                     Logger.d(tag, "code=" + intent.authentication.code)
                     Logger.d(tag, "state=" + intent.authentication.state)
                     // TODO: Use secret + authcode to generate token
+                    // TODO: Populate input fields in disabled state and show loading button
                 }
             }
         }
     }
 
-    private fun handleOnLoginClick() {
-        // TODO: Save user in db first
+    private suspend fun handleOnLoginClick() {
+        val clientId = screenState.value.clientId
+        val clientSecret = screenState.value.clientSecret
+        val serverAddress = screenState.value.serverAddress
         val state = DataFactory.createRandomString(STATE_LENGTH)
-        stateHandler.setEvent(
-            NavigateToFirefly(
-                oauthStringsProvider.getNewAccessTokenUrl(
-                    screenState.value.serverAddress,
-                    screenState.value.clientId,
-                    state
-                )
+
+        // TODO: Show Loading (Disable input, show login loading button)
+        when (
+            val result = saveUserAccountUseCase(
+                clientId, clientSecret, false, serverAddress, state
             )
-        )
+        ) {
+            is DataResult.Success -> {
+                stateHandler.setEvent(
+                    NavigateToFirefly(
+                        oauthStringsProvider.getNewAccessTokenUrl(
+                            serverAddress,
+                            clientId,
+                            state
+                        )
+                    )
+                )
+                // TODO: Hide Loading
+            }
+            is DataResult.Error -> {
+                // TODO: Show error for db save
+            }
+        }
     }
 
     private fun handleOnClientIdChange(intent: OnClientIdChange) {
