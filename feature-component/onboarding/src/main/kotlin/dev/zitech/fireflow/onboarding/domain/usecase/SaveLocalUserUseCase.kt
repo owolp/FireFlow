fb72beb8fd0178575965bf68cdea5350f7590790
Zlatin Kostov <zlatin@zitech.dev>
2023-07-04 21:51:36 +0300

    check if local username already exists before saving

--- feature-component/onboarding/src/main/kotlin/dev/zitech/fireflow/onboarding/domain/usecase/SaveLocalUserUseCase.kt
+++ feature-component/onboarding/src/main/kotlin/dev/zitech/fireflow/onboarding/domain/usecase/SaveLocalUserUseCase.kt
@@ -17,31 +17,85 @@
 
 package dev.zitech.fireflow.onboarding.domain.usecase
 
+import dev.zitech.fireflow.common.domain.repository.user.UserRepository
 import dev.zitech.fireflow.common.domain.usecase.user.SaveUserUseCase
 import dev.zitech.fireflow.core.result.OperationResult
 import javax.inject.Inject
 
+/**
+ * Use case class for saving a local user.
+ *
+ * This class handles the logic for saving a local user by generating a random username,
+ * checking if the username already exists, and then saving the user if it doesn't.
+ *
+ * @property getRandomUsernameUseCase The use case for generating a random username.
+ * @property saveUserUseCase The use case for saving a user.
+ * @property userRepository The repository for user data.
+ */
 internal class SaveLocalUserUseCase @Inject constructor(
     private val getRandomUsernameUseCase: GetRandomUsernameUseCase,
-    private val saveUserUseCase: SaveUserUseCase
+    private val saveUserUseCase: SaveUserUseCase,
+    private val userRepository: UserRepository
 ) {
 
-    suspend operator fun invoke(): OperationResult<Unit> {
-        return when (val userNameResult = getRandomUsernameUseCase()) {
-            is OperationResult.Failure -> OperationResult.Failure(userNameResult.error)
+    /**
+     * Invokes the save local user use case.
+     *
+     * @return An [OperationResult] indicating the success or failure of the operation.
+     */
+    suspend operator fun invoke(): OperationResult<Unit> =
+        when (val result = createRandomUsername()) {
+            is OperationResult.Success -> handleCreateUsernameSuccess(result.data)
+            is OperationResult.Failure -> OperationResult.Failure(result.error)
+        }
+
+    /**
+     * Creates a random username.
+     *
+     * @return An [OperationResult] containing the randomly generated username
+     *         or an error in case of failure.
+     */
+    private suspend fun createRandomUsername(): OperationResult<String> =
+        when (val usernameResult = getRandomUsernameUseCase()) {
+            is OperationResult.Success -> handleRandomUsernameSuccess(usernameResult.data)
+            is OperationResult.Failure -> OperationResult.Failure(usernameResult.error)
+        }
+
+    /**
+     * Handles the success case of creating a username.
+     *
+     * @param username The generated username.
+     * @return An [OperationResult] indicating the success or failure of the operation.
+     */
+    private suspend fun handleCreateUsernameSuccess(username: String): OperationResult<Unit> =
+        when (
+            val saveUserResult = saveUserUseCase(
+                connectivityNotification = false,
+                isCurrentUser = true,
+                email = username,
+                state = ""
+            )
+        ) {
+            is OperationResult.Success -> OperationResult.Success(Unit)
+            is OperationResult.Failure -> OperationResult.Failure(saveUserResult.error)
+        }
+
+    /**
+     * Handles the success case of generating a random username.
+     *
+     * @param username The generated username.
+     * @return An [OperationResult] containing the username if it doesn't already exist,
+     *         or creates a new random username if it exists, or an error in case of failure.
+     */
+    private suspend fun handleRandomUsernameSuccess(username: String): OperationResult<String> =
+        when (val result = userRepository.checkUserExistsByEmail(username)) {
             is OperationResult.Success -> {
-                when (
-                    val saveUserResult = saveUserUseCase(
-                        connectivityNotification = false,
-                        isCurrentUser = true,
-                        email = userNameResult.data,
-                        state = ""
-                    )
-                ) {
-                    is OperationResult.Success -> OperationResult.Success(Unit)
-                    is OperationResult.Failure -> OperationResult.Failure(saveUserResult.error)
+                if (result.data) {
+                    createRandomUsername()
+                } else {
+                    OperationResult.Success(username)
                 }
             }
+            is OperationResult.Failure -> OperationResult.Failure(result.error)
         }
-    }
 }
